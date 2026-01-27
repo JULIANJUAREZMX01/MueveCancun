7e02f1b3d0bfa178f8a8cd67ccec6201df1b98de
sistemascancunjefe-ai <sistemascancunjefe@gmail.com>
2026-01-27 14:59:37 -0500

    chore: wipe legacy redundant folders and zip file

--- MOVICUN_3.0/jules_session_15744161379540395713 (1)/rust-wasm/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn calculate_route(start_lat: f64, start_lng: f64, end_lat: f64, end_lng: f64) -> String {
-    format!("Calculando ruta de {},{} a {},{}", start_lat, start_lng, end_lat, end_lng)
-}
